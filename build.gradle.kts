d3d72f93971857dfe0b2812b21acff82944eda96
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-21 10:58:17 +0100

    build: disable publishing for jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
-import org.gradle.api.publish.PublishingExtension
-
-apply(from = "generate-metrics.gradle.kts")
-apply(from = "increment-version.gradle.kts")
-
 plugins {
     `java-library`
-    `maven-publish`
-    signing
 }
 
 allprojects {
